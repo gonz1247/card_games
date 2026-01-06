657efa394835b0520f1d41b5c4bbaee3b838656a
Gonzo <agonzalez1043@gmail.com>
2026-01-05 19:04:00 -0800

    in progress implementation of rat screw game logic

---
+++ RatScrewObjects/__init__.py
@@ -0,0 +1,4 @@
+from RatScrewObjects.RatScrewPlayer import RatScrewPlayer
+from RatScrewObjects.RatScrewGame import RatScrewGame
+
+__all__ = [RatScrewPlayer, "RatScrewGame"]
---
+++ RatScrewObjects/test_RatScrewPlayer.py
@@ -0,0 +1,54 @@
+import pytest
+
+from RatScrewObjects.RatScrewPlayer import RatScrewPlayer
+
+
+class TestRatScrewPlayer:
+    """
+    Test functionality of RatScrewPlayer class.
+    """
+
+    def test_is_valid_action_key_with_no_constraints(self):
+        """
+        Test is_valid_action_key static method of RatScrewPlayer when constraints are not applied for which keys are invalid.
+        """
+        # Test with no constraints on which keys are invalid
+        assert RatScrewPlayer.is_valid_action_key("a") is True
+        assert RatScrewPlayer.is_valid_action_key("ab") is False
+
+    def test_is_valid_action_key_with_constraints(self):
+        """
+        Test is_valid_action_key static method of RatScrewPlayer when constraints are applied for which keys are invalid.
+        """
+
+        # Test with constraints on which keys are invalid
+        invalid_keys = {"x", "y", "z"}
+        assert RatScrewPlayer.is_valid_action_key("x", invalid_keys) is False
+        assert RatScrewPlayer.is_valid_action_key("a", invalid_keys) is True
+        assert RatScrewPlayer.is_valid_action_key("yz", invalid_keys) is False
+        assert RatScrewPlayer.is_valid_action_key("bc", invalid_keys) is False
+
+    def test_init(self, monkeypatch, invalid_action_keys=None):
+        """
+        Test initialization of RatScrewPlayer.
+        """
+
+        # sequence of user inputs for play and slap keys, invalid input followed by valid input
+        user_inputs = ["foo", "a", "bar", "b"]
+        inputs = iter(user_inputs)
+
+        # Patch the built-in 'input' function to use the iterator
+        monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+        player = RatScrewPlayer(invalid_action_keys)
+        assert player.play_key == user_inputs[1]
+        assert player.slap_key == user_inputs[3]
+        assert player.card_stack.nCards == 0
+
+    def test_init_with_existing_invalid_keys(self, monkeypatch):
+        """
+        Add test coverage for initialization of RatScrewPlayer with existing invalid action keys.
+        """
+        # pre-existing invalid keys that will be checked against during initialization
+        existing_invalid_keys = {"x", "y", "z"}
+        # rerun the init test with these existing invalid keys
+        self.test_init(monkeypatch, existing_invalid_keys)
