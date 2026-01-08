52825c99240a7f6da88a95c01d3d2cde6afb6cad
Gonzo <agonzalez1043@gmail.com>
2026-01-07 18:33:01 -0800

    added class to handle logic of stack of cards that get added to during a round of Rat Screw

---
+++ RatScrewObjects/RoundCardStack.py
@@ -0,0 +1,59 @@
+from CardDeckObjects import Card, CardDeck
+
+
+class RoundCardStack:
+    """
+    Class to manage the stack of cards played during a round of Rat Screw
+    """
+
+    _face_card_countdown_cypher = {"jack": 1, "queen": 2, "king": 3, "ace": 4}
+
+    def __init__(self) -> None:
+        """
+        Initialize instance of RoundCardStack
+        """
+        self.played_card_stack = CardDeck(nDecks=0)
+        self.penalty_card_stack = CardDeck(nDecks=0)
+        self.need_face_card = False
+        self.face_card_countdown = 0
+
+    def add_played_card(self, card: Card) -> None:
+        """
+        Play a card onto the round stack and update face card countdown if necessary
+
+        Parameters
+        ----------
+        card: Card
+            The card to be played onto the round stack.
+        """
+        self.played_card_stack.add_card(card)
+        # Check if played card is a face card
+        if card.isFaceCard():
+            self.need_face_card = True
+            self.face_card_countdown = self._face_card_countdown_cypher[card.value]
+        else:
+            self.face_card_countdown -= 1
+
+    def add_penalty_card(self, card) -> None:
+        """
+        Add penalty cards to the penalty stack
+
+        Parameters
+        ----------
+        card: Card
+            Card to be added to the penalty stack.
+        """
+        self.penalty_card_stack.add_card(card)
+
+    def has_stack_been_won(self) -> bool:
+        """
+        Determine stack has been won by a player
+
+        Returns
+        -------
+        Boolean indicating whether the stack has been won
+        """
+        if self.need_face_card is False:
+            return False
+        return self.face_card_countdown < 1
+
---
+++ RatScrewObjects/test_RoundCardStack.py
@@ -0,0 +1,67 @@
+import pytest
+from CardDeckObjects.Card import Card
+from RatScrewObjects.RoundCardStack import RoundCardStack
+
+
+class TestRoundCardStack:
+    """
+    Test functionality of RoundCardStack class.
+    """
+
+    def test_add_played_card_face_card(self):
+        """
+        Test add_played_card method when a face card is played.
+        """
+        stack = RoundCardStack()
+        face_card = Card("jack", "hearts")
+        stack.add_played_card(face_card)
+        assert stack.need_face_card is True
+        assert stack.face_card_countdown == 1  # Jack allows 1 chance
+        assert stack.played_card_stack.nCards == 1
+        assert stack.penalty_card_stack.nCards == 0
+
+    def test_add_played_card_non_face_card(self):
+        """
+        Test add_played_card method when a non-face card is played.
+        """
+        countdown_default = 0
+        stack = RoundCardStack()
+        assert stack.face_card_countdown == countdown_default
+        non_face_card = Card("7", "clubs")
+        stack.add_played_card(non_face_card)
+        assert stack.need_face_card is False
+        assert stack.face_card_countdown == countdown_default - 1
+        assert stack.played_card_stack.nCards == 1
+        assert stack.penalty_card_stack.nCards == 0
+
+    def test_add_penalty_card(self):
+        """
+        Test add_penalty_card method.
+        """
+        stack = RoundCardStack()
+        penalty_card = Card("king", "spades")
+        stack.add_penalty_card(penalty_card)
+        assert stack.penalty_card_stack.nCards == 1
+        assert stack.played_card_stack.nCards == 0
+        # check that face card countdown and need_face_card remain unchanged
+        assert stack.need_face_card is False
+        assert stack.face_card_countdown == 0
+
+    def test_has_stack_been_won(self):
+        """
+        Test has_stack_been_won method.
+        """
+        stack = RoundCardStack()
+        # Initially, stack should not be won
+        assert stack.has_stack_been_won() is False
+
+        # Simulate playing a face card and countdown reaching zero
+        face_card = Card("queen", "diamonds")
+        stack.add_played_card(face_card)
+        stack.face_card_countdown = 0  # Manually set countdown to 0
+        assert stack.has_stack_been_won() is True
+
+        # Reset and test when countdown is still positive
+        stack = RoundCardStack()
+        stack.add_played_card(face_card)
+        assert stack.has_stack_been_won() is False
